a984a052181ddfa34a8cd73988d98e57cf8b8bf2
proxit-git <215718473+proxit-git@users.noreply.github.com>
2025-08-05 12:56:52 +0330

    Update [[catchall]].js

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -1,5 +1,5 @@
-const doh = 'https://1.1.1.2/dns-query'
-const dohjson = 'https://1.1.1.2/dns-query'
+const doh = 'https://1.1.1.1/dns-query'
+const dohjson = 'https://1.1.1.1/dns-query'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
